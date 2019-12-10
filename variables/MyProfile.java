2d36f0a16745d9896164b3d828bf9d5cc46a579b
Carl Kakisis <Messinias1@gmail.com>
2019-12-10 01:08:38 -0500

    variables section complete

---
+++ variables/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Carl";
+        int age = 25;
+        double desiredSalary = 75000.00;
+        char gender = 'm';
+        boolean lookingForJob = true;
+    }
+}
